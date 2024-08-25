2fb0a2a885f464903b818f4d1c3b79e1e5ed55cd
zonggonau <cristoperzonggonau@gmail.com>
2024-08-25 23:52:03 +0900

    upgrade

---
+++ app/Views/config/navbar.php
@@ -0,0 +1,44 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+        <a href="/" class="logo d-flex align-items-center me-auto me-lg-0">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/IJ.png" alt=""> -->
+            <h1 class="sitename">PEMDA INTANJAYA</h1>
+            <!-- <span>.</span> -->
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="active">HOME<br></a></li>
+                <li class="dropdown"><a href="#"><span>PROFILE</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                        <li><a href="#">Dropdown 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                            <ul>
+                                <li><a href="#">Deep Dropdown 1</a></li>
+                                <li><a href="#">Deep Dropdown 2</a></li>
+                                <li><a href="#">Deep Dropdown 3</a></li>
+                                <li><a href="#">Deep Dropdown 4</a></li>
+                                <li><a href="#">Deep Dropdown 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Dropdown 2</a></li>
+                        <li><a href="#">Dropdown 3</a></li>
+                        <li><a href="#">Dropdown 4</a></li>
+                    </ul>
+                </li>
+                <li><a href="#about">DOKUMEN PUBLIKASI</a></li>
+                <li><a href="#services">E-GOVERMENT</a></li>
+                <li><a href="#portfolio">BERITA</a></li>
+                <li><a href="#team">GALERI</a></li>
+
+                <li><a href="#contact">KONTAK</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+        <!-- <a class="btn-getstarted" href="index.html#about">Get Started</a> -->
+
+    </div>
+</header>
