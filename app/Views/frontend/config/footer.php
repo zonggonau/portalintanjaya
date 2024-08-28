5d5c7e445b1d76d0ccc2fedf384123b8baa41ac0
zonggonau <cristoperzonggonau@gmail.com>
2024-08-28 18:35:57 +0900

    upgrade

--- app/Views/frontend/config/footer.php
+++ app/Views/frontend/config/footer.php
@@ -60,13 +60,13 @@
 
     <div class="copyright">
         <div class="container text-center">
-            <p>© <span>Copyright</span> <strong class="px-1 sitename">GP</strong> <span>All Rights Reserved</span></p>
+            <p>© <span>Copyright</span> <strong class="px-1 sitename">PEMDA INTANJAYA</strong> <span>All Rights Reserved</span></p>
             <div class="credits">
                 <!-- All the links in the footer should remain intact. -->
                 <!-- You can delete the links only if you've purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
             </div>
         </div>
     </div>
